86a1b1cb3848a90d46e073754f92c92d7facbd65
Atr24o <arthurhenriuque@gmail.com>
2025-09-08 15:51:51 -0300

    Update CalculaVerso(1).py

--- CalculaVerso(1).py
+++ CalculaVerso(1).py
@@ -4,7 +4,8 @@ projeto de criação de calculadora do Dio
 
 O Projeto do grupo Programa Verseo é uma calculadora com fuincionaliades simples e práticas que consigam realizar calculos de forma rápida e simples, visando agilizar e dar assistencia a nossos usuarios, saslvando seu tempo dos monstros de 7 cabeças que as contas podem ser
 
-aqui está uma apresentação que mostra a evolução de nosso projeto:<object data="Green and Grey Simple Nature Presentation (3).pdf" width="1000" height="1000" type='application/pdf'></object>
+aqui está uma apresentação que mostra a evolução de nosso projeto <embed src="Green%20and%20Grey%20Simple%20Nature%20Presentation%20(3).pdf" width="600" height="400" type="application/pdf">
+
 
 **integrantes:**
 - <a href="https://github.com/Atr24o" target="_blank">Arthur Henrique</a> (Project Owner)
